3c4d600f598a21135f24b6afcef08a4dec714b76
Mrduck <tuanduy.221007@gmail.com>
2025-09-21 13:15:48 +0700

    change 1

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.querySelector(".button_toggle").addEventListener('click', function(){
-    document.body.classList.toggle('dark-btn');
-})
+const toggleBtn = document.querySelector(".button_toggle button");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  toggleBtn.textContent = "Light Mode";
+} else {
+  toggleBtn.textContent = "Dark Mode";
+}
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    toggleBtn.textContent = "Light Mode";
+    localStorage.setItem("theme", "dark");
+  } else {
+    toggleBtn.textContent = "Dark Mode";
+    localStorage.setItem("theme", "light");
+  }
+});
